7497841db9b71163c1a98a067f3db2e5d4e8724a
drabiot <tchartie@student.42angouleme.fr>
2026-05-11 20:30:29 +0200

    Improved register initialization to 0

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -2,13 +2,13 @@ section .text
 global ft_strlen
 
 ft_strlen:
-	mov rax, 0					;create a 64-bits register that will be our size_t return value
+	xor rax, rax			;create a 64-bits register that will be our size_t return value
 
 .loop:
 	cmp byte [rdi + rax], 0	;compare the byte located at address (dest + <our 64-bits regitser>) with \0
-	je .done						;if the byte is \0 run .done
-	inc rax						;increase our return value
-	jmp .loop					;jump directly on .loop
+	je .done				;if the byte is \0 run .done
+	inc rax					;increase our return value
+	jmp .loop				;jump directly on .loop
 
 .done:
    ret
